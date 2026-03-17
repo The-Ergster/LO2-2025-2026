9957ac9c90fec0cd4920b88b590e187517e161be
Snowpelt <shawn.sz.yu@gmail.com>
2026-03-17 19:22:34 -0400

    Update jackpractice.java
    Added more code

--- TeamCode/src/main/java/LO2/Practice/Jackpractice.java
+++ TeamCode/src/main/java/LO2/Practice/Jackpractice.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import com.qualcomm.robotcore.hardware.CRServo;
 
+import java.lang.Math;
 
 @TeleOp
 public class jackpractice extends OpMode{
@@ -19,7 +20,7 @@ public class jackpractice extends OpMode{
 @Override
   public void init(){
     
-    frontLeft = hardwareMap.get(DcMotorEx.class, "fl");
+     frontLeft = hardwareMap.get(DcMotorEx.class, "fl");
     frontRight = hardwareMap.get(DcMotorEx.class, "fr");
     backLeft = hardwareMap.get(DcMotorEx.class, "bl");
     backRight = hardwareMap.get(DcMotorEx.class, "br");
@@ -33,9 +34,51 @@ public class jackpractice extends OpMode{
     flywheelLeft.setDirection(DcMotorEx.Direction.FORWARD); 
     flywheelRight.setDirection(DcMotorEx.Direction.REVERSE);
   }
-@Override
+public void driveOmni(double y, double rx, double x){
+
+  final double MTPS = 4661;
+
+  double maxValue = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+  double flpower = (y + x + rx) / maxValue;
+  double blpower = (y - x + rx) / maxValue;
+  double frpower = (y - x - rx) / maxValue;
+  double brpower = (y + x - rx) / maxValue;
+  
+  frontLeft.setVelocity(flPower * MTPS);
+  frontRight.setVelocity(frPower * MTPS);
+  backLeft.setVelocity(blPower * MTPS);
+  backRight.setVelocity(brPower * MTPS);
+}  
+  
+  @Override
   public void loop() {
+   
+    double y = gamepad1.left_stick_y;
+    double x = gamepad1.left_stick_x;
+    double rx = gamepad1.right_stick_x;
+    driveOmni(y, x, rx);
 
+    if (gamepad1.x){
+      flywheelRight.setPower(1);
+      flywheelLeft.setPower(1);
+      try{
+        Thread.sleep(1500)
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }  
+      loaderServo.setPower(1)
+    } else if (gamepad1.y){
+     //Makes the flywheel motors go in opposite directions
+      try{
+        Thread.sleep(1500)
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      } 
+      flywheelLeft.setPower(-1);
+      flywheelRight.setPower(1);
+  } else{
+    flywheelRight.setPower(0);
+    flywheelLeft.setPower(0);
+    loaderServo.setPower(0); 
   }
-
-}
+}    
