328b5ae221350262b7eb09c4568ddef2dfe1f767
Devik100 <74122137+Devik100@users.noreply.github.com>
2026-03-17 18:24:44 -0400

    Update Devikpractice.java
    I lowk wrote the code fr,

--- TeamCode/src/main/java/LO2/Practice/Devikpractice.java
+++ TeamCode/src/main/java/LO2/Practice/Devikpractice.java
@@ -1,1 +1,35 @@
 // Wawa
+package LO2.Practice;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+@TeleOp
+public class code extends OpMode{
+  private DcMotorEx frontLeft, frontRight, backLeft, backRight;
+  private DcMotorEx flywheelRight, flywheelLeft;
+  private CRServo loaderServo;
+}
+@Override
+  public void init(){
+    frontLeft = hardwareMap.get(DcMotorEx.class, "fl");
+    frontRight = hardwareMap.get(DcMotorEx.class, "fr");
+    backLeft = hardwareMap.get(DcMotorEx.class, "bl");
+    backRight = hardwareMap.get(DcMotorEx.class, "br");
+    flywheelRight = hardwareMap.get(DcMotorEx.class, "wr");
+    flywheelLeft = hardwareMap.get(DcMotorEx.class, "wl");
+
+    frontLeft.setDirection(DcMotorEx.Direction.REVERSE);
+    backLeft.setDirection(DcMotorEx.Direction.REVERSE);
+    frontRight.setDirection(DcMotorEx.Direction.FORWARD);
+    backRight.setDirection(DcMotorEx.Direction.FORWARD);
+    flywheelLeft.setDirection(DcMotorEx.Direction.FORWARD);
+    flywheelRight.setDirection(DcMotorEx.Direction.REVERSE);
+}
+
+@Override
+  public void loop(){
+    // Wawa
+}
