fbdd40aad19926bc0fcf727582642fe33f1f1622
idkwhatimdealingwith <quinnsidell@icloud.com>
2026-03-17 19:22:06 -0400

    Update QuinnPractaice.java
       // GREAD SECOND \\ 
    // GOD DAM THE SUN \\
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    The robot KILLS ITS SELF

--- TeamCode/src/main/java/LO2/Practice/QuinnPractaice.java
+++ TeamCode/src/main/java/LO2/Practice/QuinnPractaice.java
@@ -8,6 +8,8 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import com.qualcomm.robotcore.hardware.CRServo; 
 
+import java.lang.Math;
+
 
 
 @TeleOp
@@ -29,10 +31,59 @@ public class QuinnPractaice extends Opmode{
       frontRight.setDiection(DcMotorEx.Direction.FORWARD);
       backLeft.setDiection(DcMotorEx.Direction.REVERSE);
       backRight.setDiection(DcMotorEx.Direction.FORWARD);
+      flywheelLeft.setDiection(DcMotorEx.Direction.FORWARD);
+      flywheelRight.setDiection(DcMotorEx.Direction.REVERSE);
     }
+  public void driveOmni(double y, double rx, double x) {
+
+    final double MTPS = 4661;
+
+    double maxVal = Math.max(Math.abs(y) + Math.abs(rx) + Math.abs(x), 1);
+    double flPower = (y + x + rx) / maxValue;
+    double blPower = (y - x + rx) / maxValue;
+    double frPower = (y - x + rx) / maxValue;
+    double brPower = (y + x + rx) / maxValue;
+
+    frontLeft.setVelocity(flPower * MTPS); 
+    backLeft.setVelocity(blPower * MTPS);
+    frontRight.setVelocity(frPower * MTPS);
+    backRight.setVelocity(brPower * MTPS);
+  }
+  
   @Override
     public void loop(){
 
+      double y = gamepad1.left_stick_y; 
+      double rx = gamepad1.right_stick_x;
+      double x = gamepad1.left_stick_x;
+      driveOmni(y, rx, x); 
+      
+      if (gamepad1.x) { 
+          flywheelRight.setPower(1);
+          flywheelLeft.setPower(1);
+        // YOU SHOULD KILL YOUR SELF NOW (to the robot)
+        try{
+           Thread.sleep(1500)
+        } catch (InterruptedException e) {
+            throw new runtimeException(e);
+        }
+        loaderServo.setPower(1);
+      } else if {
+        loaderServo.setPower(-1)
+           // GREAD SECOND \\
+          // GOD DAM THE SUN \\
+        try{
+           Thread.sleep(999999999999999999999999) // also GOD DAM THE SUN \\ 
+        } catch (InterruptedException e) {
+            throw new runtimeException(e);
+        }
+        loaderServo.setPower(1);
+      } else {
+        flywheelRight.setPower(0);
+          flywheelLeft.setPower(0);
+        loaderServo.setPower(0);
+      }
+      
     }
 
   
