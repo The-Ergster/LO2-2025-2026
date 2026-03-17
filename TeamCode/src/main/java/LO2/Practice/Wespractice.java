70c14a966fb1504ef6d890dcd82738af13796968
FuryHehe <ChanWesley0703@gmail.com>
2026-03-17 19:23:07 -0400

    Update Wespractice.java
    Added public void driveOmni, public void loop, gamepad.x and Die.

--- TeamCode/src/main/java/LO2/Practice/Wespractice.java
+++ TeamCode/src/main/java/LO2/Practice/Wespractice.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import com.qualcomm.robotcore.hardware.CRServo;
 
+import java.lang.Math;
 
 @TeleOp
 public class Wespractice extends OpMode{
@@ -27,15 +28,81 @@ public class Wespractice extends OpMode{
     flywheelRight = hardwareMap.get(DcMotorEx.class, "wr");
 
     frontLeft.setDirection(DcMotorEx.Direction.REVERSE);
-    baclLeft.setDirection(DcMotorEx.Direction.REVERSE);
-    backLeft.setDirection(DcMotorEx.Direction.FORWARD);
+    backLeft.setDirection(DcMotorEx.Direction.REVERSE);
+    frontRight.setDirection(DcMotorEx.Direction.FORWARD);
     backRight.setDirection(DcMotorEx.Direction.FORWARD);
-    flywheelLEFT.setDirection(DcMotorEx.Direction.FORWARD);
-    flywheelRIGHT.setDirection(DcMotorEx.Direction.REVERSE);
+    flywheelLeft.setDirection(DcMotorEx.Direction.FORWARD);
+    flywheelRight.setDirection(DcMotorEx.Direction.REVERSE);
   }
+public void driveOmni(double y, double x, double rx){
+
+  final double Max_Ticks_Per_Second = 4661;
+
+  double  maxValue = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+  double flPower = (y + x + rx) / maxValue;
+  double frPower = (y - x - rx) / maxValue;
+  double blPower = (y - x + rx) / maxValue;
+  double brPower = (y + x - rx) / maxValue;
+  
+  frontLeft.setVelocity(flPower * Max_Ticks_Per_Second);
+  backLeft.setVelocity(blPower * Max_Ticks_Per_Second);
+  frontRight.setVelocity(frPower * Max_Ticks_Per_Second);
+  backRight.setVelocity(brPower * Max_Ticks_Per_Second);
+  }
+  
 @Override
   public void loop(){
 
+    double y = gamepad1.left_stick_y;
+    double x = gamepad1.left_stick_x;
+    double rx = gamepad1.right_stick_x;
+    driveOmni(y, x, rx);
+
+    if (gamepad1.x){
+      flywheelLeft.setPower(1);
+      flywheelRight.setpower(1);
+      try{
+        Thread.sleep(1500);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      loaderServo.setPower(1);
+    } else if (gamepad1.x){
+      //Die
+      frontLeft.setPower(1);
+      backleft.setPower(1);
+      frontBack.setpower(-1);
+      backRight.setpower(1);
+      flywheelLeft.setPower(-1);
+      flywheelRight.setPower(1);
+      loaderServo.setPower(-1);
+      try{
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      backRight.setpower(-1);
+      flywheelLeft.setPower(1);
+      flywheelRight.setPower(-1);
+      loaderServo.setPower(1);
+      try{
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      frontLeft.setPower(0);
+      backleft.setPower(0);
+      frontBack.setpower(0);
+      backRight.setpower(0);
+      flywheelLeft.setPower(0);
+      flywheelRight.setPower(0);
+      loaderServo.setPower(0);
+    } else{
+      flywheelLeft.setPower(0);
+      flywheelRight.setPower(0);
+      loaderServo.setpower(0);
+      
+    }
   }
   
 }
